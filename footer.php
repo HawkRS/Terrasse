a6e2d999d9424dce69101f6deab303b622d15a4c
DanteCero <zeroakron@gmail.com>
2016-10-12 15:48:18 -0500

    Archivo que contiene el footer para todas las paginas y solo tener que actualizar un archivo en lugar de todas las paginas

---
+++ footer.php
@@ -0,0 +1,58 @@
+<div class="INFO">
+<div class="FCont" id="Mapa">
+  <h5>MAPA DEL SITIO</h5>
+  <ul class="FMenu">
+    <li class="FLink">      <a href="index.php">Inicio</a></li>
+    <li class="FLink">   <a href="asadores.php">Asadores</a></li>
+    <li class="FLink">    <a href="firepit.php">Firepit</a></li>
+    <li class="FLink">    <a href="gazebos.php">Gazebos</a></li>
+    <li class="FLink"> <a href="accesorios.php">Accesorios</a></li>
+    <li class="FLink">    <a href="galeria.php">Galeria</a></li>
+    <li class="FLink">   <a href="contacto.php">Contacto</a></li>
+    <li class="FLink">    <a href="gazebos.php">Mi cuenta</a></li>
+    <li class="FLink"> <a href="accesorios.php">Preguntas frecuentes</a></li>
+    <li class="FLink">    <a href="galeria.php">Politicas</a></li>
+    <li class="FLink">   <a href="contacto.php">Aviso legal</a></li>
+  </ul>
+</div>
+
+<div class="FCont" id="Cuenta">
+  <h5>MI CUENTA EN LINEA</h5>
+  <ul class="FMenu">
+    <li class="ULink">    <a href="#">Mi perfil</a></li>
+    <li class="ULink">    <a href="#">Ajustes</a></li>
+    <li class="ULink">    <a href="#">Mi carito</a></li>
+    <li class="ULink">    <a href="#">Historial de compras</a></li>
+  </ul>
+</div>
+<div class="FCont" id="Cont">
+  <h5>CONTACTO</h5>
+  <div class="Conta">
+    <p>Lauro Badillo Diaz #420</p>
+    <p>Col. Brisas de Chapala</p>
+    <p>Tlaquepaque, Jalisco</p>
+    <p>Telefonos:  36-70-69-91  |  333-968-9393</p>
+    <p>Ventas@Terrasse.com.mx</p>
+    <h5>SIGUENOS</h5>
+    <a href="#"><img src="img/RS/face.png" alt="Facebook" id="RFace"/></a>
+    <a href="#"><img src="img/RS/youtube.png" alt="Youtube" id="RYouT"/></a>
+    <a href="#"><img src="img/RS/insta.png" alt="Instagram" id="RInsta"/></a>
+  </div>
+</div>
+
+</div>
+<div class="LEGAL">
+<div class="LT">
+  2016 Terrasse
+</div>
+<div class="LHM">
+  <p>
+    Developed by:
+  </p>
+  <img src="img/Hawk.png" alt="Hawk Media" id="HMima"/>
+  <img src="img/Roman1992.jpg" alt="Roman" id="Roman92"/>
+  <img src="img/DanteCero.jpg" alt="Dante" id="Dante0"/>
+  <p>Hawk Media | Romanbr1992 | DanteCero</p>
+  <li class="Team"><a href="equipo.php"><h5>ACERCA DE NOSOTROS</h5></a></li>
+</div>
+</div>
