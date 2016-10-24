245a8c05ebc94c80bab32ca0cd5b2e492d4049dc
DanteCero <zeroakron@gmail.com>
2016-10-24 18:05:50 -0500

    Se actualizo la ruta para las imágenes de los integrantes así como el logo de la página web

--- equipo.php
+++ equipo.php
@@ -15,7 +15,7 @@
       <section class="MSection">
         <!--INICIA SLIDER-->
         <div class="Slider">
-          <img src="imagenes/terrasse.png" alt="SLIDER" id="Slider"/>
+          <img src="img/logos/terrasse.png" alt="Diseño y equipo para terrazas" id="logo"/>
         </div>
         <!--INICIA INFO DE PAGINA WEB-->
         <div class="inside">
@@ -28,17 +28,17 @@
         <div class="contform">
             <h2 id="h2form">Nuestro Equipo</h2>
 				<ul class="team">
-                    <li><img src="imagenes/gara.jpg"><strong>Integrante 01: HawkRs</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-				    <li><img src="imagenes/king-bradley.jpg"><strong>Integrante 02: Romanbr1992</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-				    <li class="last"><img src="imagenes/escanor.jpg"><strong>integrante 03: DanteCero</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                </ul>
+          <li><img src="img/Hawk.png"><strong>Integrante 01: HawkRs</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+			    <li><img src="img/Roman1992.jpg"><strong>Integrante 02: Romanbr1992</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+			    <li><img src="img/DanteCero.jpg"><strong>integrante 03: DanteCero</strong><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+        </ul>
         </div>
         <!--TERMINA INFORMACION DEL EQUIPO-->
       </section>
     </div>
   </body>
 
-  <footer>
+  <footer class="clear">
     <?php include 'footer.php'; ?>
   </footer>
 </html>
