87fdac670030aa97c0c57ff08b35258031a1a72b
DanteCero <zeroakron@gmail.com>
2016-11-02 22:21:45 -0600

    se agrego un div para mostrar la información además de que el contenedor tiene un tamaño de 90% solo para dar un mejor estilo al sitio web

--- accesorios.php
+++ accesorios.php
@@ -7,41 +7,43 @@
     <?php include 'head.php'; ?>
   </head>
   <body>
-     <!--INICIA SECCION PRINCIPAL-->
-      <section class="MSection">
-        <div class="LAsadores">
-          <div class="AIndividual">
-            <img class="Gazima" src="img/accesorios/escala/XYexteriores1.jpg" alt="TG001" id="Gazima"/>
-            <div class="ADes">
-              <h5>Sillas color neón modelo TAD001</h5>
-              <h4>$2000 MXN</h4>
-              <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
-              <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
+     <div class="info">
+       <!--INICIA SECCION PRINCIPAL-->
+        <section class="MSection">
+          <div class="LAsadores">
+            <div class="AIndividual">
+              <img class="Gazima" src="img/accesorios/escala/XYexteriores1.jpg" alt="TG001" id="Gazima"/>
+              <div class="ADes">
+                <h5>Sillas color neón modelo TAD001</h5>
+                <h4>$2000 MXN</h4>
+                <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
+                <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
+              </div>
+            </div>
+            <div class="AIndividual">
+              <img class="Gazima" src="img/accesorios/escala/XYexteriores2.jpg" alt="TG001" id="Gazima"/>
+              <div class="ADes">
+                <h5>Juego de sillas con mesa modelo TAD002</h5>
+                <h4>$1200 MXN</h4>
+                <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
+                <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
+              </div>
+            </div>
+            <div class="AIndividual">
+              <img class="Gazima" src="img/accesorios/escala/XYexteriores3.jpg" alt="TG001" id="Gazima"/>
+              <div class="ADes">
+                <h5>Juego de sillones modelo TAD003</h5>
+                <h4>$2000 MXN</h4>
+                <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
+                <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
+              </div>
             </div>
           </div>
-          <div class="AIndividual">
-            <img class="Gazima" src="img/accesorios/escala/XYexteriores2.jpg" alt="TG001" id="Gazima"/>
-            <div class="ADes">
-              <h5>Juego de sillas con mesa modelo TAD002</h5>
-              <h4>$1200 MXN</h4>
-              <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
-              <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
-            </div>
-          </div>
-          <div class="AIndividual">
-            <img class="Gazima" src="img/accesorios/escala/XYexteriores3.jpg" alt="TG001" id="Gazima"/>
-            <div class="ADes">
-              <h5>Juego de sillones modelo TAD003</h5>
-              <h4>$2000 MXN</h4>
-              <p>Asador tipo argentino fabricado en acero negro e inoxidable</p>
-              <p>Color negro azabache mas informacion <span class="ArtInfo"><a href="#">AQUI</a></span></p>
-            </div>
-          </div>
-        </div>
 
-        <!--INICIA INFO DE LA EMPRESA-->
-      </section>
-    </div>
+          <!--INICIA INFO DE LA EMPRESA-->
+        </section>
+      </div>
+     </div>
   </body>
 
   <footer class="clear">
--- contacto.php
+++ contacto.php
@@ -17,76 +17,77 @@
         <img src="img/carrusel/contacto/03.jpg" alt="#" />
         <img src="img/carrusel/contacto/04.jpg" alt="#" />
       </div>
-
     </div>
-      <!--INICIA SECCION PRINCIPAL-->
-      <section class="MSection">
-        <!--INICIA INFO DE CONTACTO-->
-        <div class="inside">
-            <h2>Nuestro Contacto</h2>
-				<div class="NContact">
-				    <address class="span">
-            <p>Lauro Badillo Diaz #420</p>
-            <p>Col. Brisas de Chapala</p>
-            <p>Tlaquepaque, Jalisco</p>
-            <p>Telefonos:  36-70-69-91  |  333-968-9393</p>
-            <p>Ventas@Terrasse.com.mx</p>
-				    </address>
-				    <p class="EXTInfo"><b>Mas Información:</b><br>
-				        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				</div>
+    
+    <div class="info">
+        <!--INICIA SECCION PRINCIPAL-->
+        <section class="MSection">
+          <!--INICIA INFO DE CONTACTO-->
+          <div class="inside">
+              <h2>Nuestro Contacto</h2>
+  				<div class="NContact">
+  				    <address class="span">
+              <p>Lauro Badillo Diaz #420</p>
+              <p>Col. Brisas de Chapala</p>
+              <p>Tlaquepaque, Jalisco</p>
+              <p>Telefonos:  36-70-69-91  |  333-968-9393</p>
+              <p>Ventas@Terrasse.com.mx</p>
+  				    </address>
+  				    <p class="EXTInfo"><b>Mas Información:</b><br>
+  				        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  				</div>
+            </div>
+          <!--TERMINA INFO DE CONTACTO-->
+          <!--INICIA FORMULARIO DE CONTACTO-->
+          <div class="contform">
+          <h2 id="h2form">Formulario de Contacto</h2>
+              <form id="contacts-form" action="." oninput="range_control_value.value = range_control.valueAsNumber">
+  				<fieldset>
+  				    <div class="campo">
+  				        <label>Nombre: </label>
+  				        <input title="Se Necesita un Nombre" type="text" value="" required />
+  				    </div>
+  				    <div class="campo">
+  				        <label>E-mail: </label>
+  				        <input type="email" value="" required />
+  				    </div>
+  				    <div class="campo">
+  				        <label>Website:    </label>
+  				        <input type="text" value="" placeholder="Ingresa la URL de tu Página" />
+  				    </div>
+  				    <div class="campo">
+  				        <label>Mensaje:    </label>
+  				        <textarea cols="1" rows="1" required></textarea>
+  				    </div>
+                      <button id="boton" type="submit">Enviar Mensaje</button>
+  				</fieldset>
+              </form>
           </div>
-        <!--TERMINA INFO DE CONTACTO-->
-        <!--INICIA FORMULARIO DE CONTACTO-->
-        <div class="contform">
-        <h2 id="h2form">Formulario de Contacto</h2>
-            <form id="contacts-form" action="." oninput="range_control_value.value = range_control.valueAsNumber">
-				<fieldset>
-				    <div class="campo">
-				        <label>Nombre: </label>
-				        <input title="Se Necesita un Nombre" type="text" value="" required />
-				    </div>
-				    <div class="campo">
-				        <label>E-mail: </label>
-				        <input type="email" value="" required />
-				    </div>
-				    <div class="campo">
-				        <label>Website:    </label>
-				        <input type="text" value="" placeholder="Ingresa la URL de tu Página" />
-				    </div>
-				    <div class="campo">
-				        <label>Mensaje:    </label>
-				        <textarea cols="1" rows="1" required></textarea>
-				    </div>
-                    <button id="boton" type="submit">Enviar Mensaje</button>
-				</fieldset>
-            </form>
-        </div>
-        <!--TERMINA FORMULARIO DE CONTACTO-->
-      </section>
+          <!--TERMINA FORMULARIO DE CONTACTO-->
+        </section>
+      </div>
+
+      <!--Script para activar el carrusel-->
+      <script src="js/jquery.js"></script>
+  		<script src="js/jquery.slides.js"></script>
+  		<script>
+
+  				$(function(){
+  				$(".slides").slidesjs({
+  					play: {
+  						active: true,
+  						effect: "slide",
+  						interval: 3000,
+  						auto: true,
+  						swap: true,
+  						pauseOnHover: false,
+  						restartDelay: 2500
+  				    }
+  				});
+  				});
+  		</script>
     </div>
-
-    <!--Script para activar el carrusel-->
-    <script src="js/jquery.js"></script>
-		<script src="js/jquery.slides.js"></script>
-		<script>
-
-				$(function(){
-				$(".slides").slidesjs({
-					play: {
-						active: true,
-						effect: "slide",
-						interval: 3000,
-						auto: true,
-						swap: true,
-						pauseOnHover: false,
-						restartDelay: 2500
-				    }
-				});
-				});
-		</script>
-
   </body>
 
   <footer class="clear">
