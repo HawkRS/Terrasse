c600cd99bbdf073c19ad3f43f5351bee7400305e
DanteCero <zeroakron@gmail.com>
2016-11-02 22:27:43 -0600

    se agrego un bloque con la clase .info para mentener un tamaño de 90%

--- index.php
+++ index.php
@@ -9,16 +9,17 @@
   </head>
 
   <body>
-
+    <!-- Comienza el corrusel de imágnes-->
     <div class="contenedor">
       <div class="slides">
-        <img src="img/carrusel/index/01.jpg" alt="" />
-        <img src="img/carrusel/index/02.jpg" alt="" />
-        <img src="img/carrusel/index/03.jpg" alt="" />
+        <img src="img/carrusel/galeria/01.jpg" alt="#" />
+        <img src="img/carrusel/galeria/02.jpg" alt="#" />
+        <img src="img/carrusel/galeria/03.jpg" alt="#" />
       </div>
-
     </div>
-        <!--INICIA BLOQUE PRINCIPAL-->
+    
+    <div class="info">
+      <!--INICIA BLOQUE PRINCIPAL-->
     <div class="bloque">
       <!--INICIA SECCION PRINCIPAL-->
       <section class="MSection">
@@ -45,49 +46,51 @@
             </div>
           </div>
         </div>
-        <!--INICIA INFO DE LA EMPRESA-->
-        <div>
-          <div class="acerca">
-            <h4>ACERCA DE NOSOTROS</h4>
-            <p class="MPInfo">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+
+          <!--INICIA INFO DE LA EMPRESA-->
+          <div>
+            <div class="acerca">
+              <h4>ACERCA DE NOSOTROS</h4>
+              <p class="MPInfo">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div class="compranos">
+              <h4>PORQUE ELEGIR A TERRASSE</h4>
+              <p class="MPInfo">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
           </div>
-          <div class="compranos">
-            <h4>PORQUE ELEGIR A TERRASSE</h4>
-            <p class="MPInfo">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
+
+      <!--Script para activar el carrusel-->
+      <script src="js/jquery.js"></script>
+  		<script src="js/jquery.slides.js"></script>
+  		<script>
+
+  				$(function(){
+  				$(".slides").slidesjs({
+  					play: {
+  						active: true,
+  						effect: "slide",
+  						interval: 3000,
+  						auto: true,
+  						swap: true,
+  						pauseOnHover: false,
+  						restartDelay: 2500
+  				    }
+  				});
+  				});
+  		</script>
     </div>
-
-    <!--Script para activar el carrusel-->
-    <script src="js/jquery.js"></script>
-		<script src="js/jquery.slides.js"></script>
-		<script>
-
-				$(function(){
-				$(".slides").slidesjs({
-					play: {
-						active: true,
-						effect: "slide",
-						interval: 3000,
-						auto: true,
-						swap: true,
-						pauseOnHover: false,
-						restartDelay: 2500
-				    }
-				});
-				});
-		</script>
   </body>
 
   <footer class="clear">
