b7428494bd793f252e8726207c6042c64b66d5a7
DanteCero <zeroakron@gmail.com>
2016-11-06 14:59:10 -0600

    se organizo las columnas, y se agregaron estilos e imagenes

--- footer.php
+++ footer.php
@@ -1,66 +1,62 @@
-<div class="INFO">
-<div class="FCont" id="Mapa">
-  <h5>MAPA DEL SITIO</h5>
-  <ul class="FMenu">
-    <li class="FLink">      <a href="index.php">Inicio</a></li>
-    <li class="FLink">   <a href="asadores.php">Asadores</a></li>
-    <li class="FLink">    <a href="firepit.php">Firepit</a></li>
-    <li class="FLink">    <a href="gazebos.php">Gazebos</a></li>
-    <li class="FLink"> <a href="accesorios.php">Accesorios</a></li>
-    <li class="FLink">    <a href="galeria.php">Galeria</a></li>
-    <li class="FLink">   <a href="contacto.php">Contacto</a></li>
-    <li class="FLink">    <a href="gazebos.php">Mi cuenta</a></li>
-    <li class="FLink"> <a href="accesorios.php">Preguntas frecuentes</a></li>
-    <li class="FLink">    <a href="galeria.php">Politicas</a></li>
-    <li class="FLink">   <a href="contacto.php">Aviso legal</a></li>
-  </ul>
-</div>
+<!-- Seccion para la cuenta que esta activa por el momento -->
+<section id="cuenta">
+  <h4>Mi cuenta</h4> <br>
+  <!-- menú de cuenta -->
+  <nav >
+    <ul>
+      <li><a href="#">Mi perfil <img src="img/perfil.png" alt="" /></a></li>
+      <li><a href="#">Ajustes <img src="img/ajuste.png" alt="" /></a></li>
+      <li><a href="#">Mi carrito <img src="img/carrito.png" alt="" /></a></li>
+      <li><a href="#">Historial de compras <img src="img/historial.png" alt="" /></a></li>
+    </ul>
+  </nav>
+</section>
 
-<div class="FCont" id="Cuenta">
-  <h5>MI CUENTA EN LINEA</h5>
-  <ul class="FMenu">
-    <li class="ULink">    <a href="#">Mi perfil</a></li>
-    <li class="ULink">    <a href="#">Ajustes</a></li>
-    <li class="ULink">    <a href="#">Mi carito</a></li>
-    <li class="ULink">    <a href="#">Historial de compras</a></li>
-  </ul>
-</div>
-<div class="FCont" id="Cont">
-  <address class="#">
-    <h5>CONTACTO</h5>
-    <div class="Conta span">
-      <p>Lauro Badillo Diaz #420</p>
-      <p>Col. Brisas de Chapala</p>
-      <p>Tlaquepaque, Jalisco</p>
-      <p>Telefonos:  36-70-69-91  |  333-968-9393</p>
-      <p>Ventas@Terrasse.com.mx</p>
-      <h5>Siguenos en:</h5>
-  </address>
+<!-- Redes sociales y dirección del establecimiento -->
+<section id="contacto">
+  <h4>Contactanos en:</h4> <br>
+  <article>
 
+    <p class="importante">
+      Dirección: Lauro Badillo Díaz #420 <br>
+      Col. Brisas de Chapala <br>
+      Tlaquepaque, Jalisco <br><br>
+      Telefonos: 36-70-69-91  | 333-968-9393 <br>
+      correo electrónico: Ventas@Terrasse.com.mx <br><br>
+    </p>
+
+    <h4>Siguenos en:</h4> <br>
     <!--Redes sociales-->
-    <a href="#"><img src="img/RS/face.png" alt="Facebook" id="Rfb"/></a>
-    <a href="#"><img src="img/RS/insta.png" alt="Instagram" id="Rinst"/></a>
-    <a href="#"><img src="img/RS/googleplus.png" alt="Google plus" id="Rg+"/></a>
-    <a href="#"><img src="img/RS/twitter.png" alt="Twitter" id="Rtw"/></a>
-    <a href="#"><img src="img/RS/telegram.png" alt="Telegram" id="Rtl"/></a>
-    <a href="#"><img src="img/RS/whatsapp.png" alt="Whatsapp" id="Rwsp"/></a>
+    <div class="contenedorR">
+      <a href="#"><img src="img/RS/face.png" alt="Facebook" id="Rfb"/></a>
+      <a href="#"><img src="img/RS/googleplus.png" alt="Google plus" id="Rg+"/></a>
+      <a href="#"><img src="img/RS/twitter.png" alt="Twitter" id="Rtw"/></a>
+      <a href="#"><img src="img/RS/telegram.png" alt="Telegram" id="Rtl"/></a>
+      <a href="#"><img src="img/RS/whatsapp.png" alt="Whatsapp" id="Rwsp"/></a>
+    </div>
 
+  </article>
+</section>
+
+<!-- Información de los desarrolladores del sitio -->
+<section id="desarrolladores">
+  <h4>Developed by:</h4>
+  <div class="contenedorD">
+
+    <div id="HMima">
+      <img src="img/Hawk.png" alt="Hawk Media"/>
+      <p>Hawk media</p>
+    </div>
+
+    <div id="Roman92">
+      <img src="img/Roman1992.jpg" alt="Roman"/>
+      <p>Roman92</p>
+    </div>
+
+   <div id="Dante0">
+      <img src="img/DanteCero.jpg" alt="Dante"/>
+      <p>DanteCero</p>
+    </div>
   </div>
-</div>
-
-</div>
-<div class="LEGAL">
-<div class="LT">
-  2016 Terrasse
-</div>
-<div class="LHM">
-  <p>
-    Developed by:
-  </p>
-  <img src="img/Hawk.png" alt="Hawk Media" id="HMima"/>
-  <img src="img/Roman1992.jpg" alt="Roman" id="Roman92"/>
-  <img src="img/DanteCero.jpg" alt="Dante" id="Dante0"/>
-  <p>Hawk Media | Romanbr1992 | DanteCero</p>
-  <li class="Team"><a href="equipo.php"><h5>ACERCA DE NOSOTROS</h5></a></li>
-</div>
-</div>
+  <a href="equipo.php"><h4>Acerca de nosotros</h4></a>
+</section>
