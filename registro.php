0e83cc51cc6224d90e39fb610fe0516b311883dc
DanteCero <zeroakron@gmail.com>
2016-11-08 00:36:39 -0600

    Se quitaron las etiquetas p del formulario ya que marcaba error al ejecutar el js

--- registro.php
+++ registro.php
@@ -20,19 +20,19 @@
         <div id="cuerpo">
             <form method="post" id="nvoUsr">
               <!-- Ingreso del nombre del usuario -->
-              <p>
+
                 <label for="nombre">Nombre:<span>*</span></label>
                 <input type="text" name="nombre" id="nombre" required="required">
-              </p>
+
 
               <!-- Nombre de usuario para entrar al sistema -->
-              <p>
+
                 <label for="usrNom">Nombre de usuario:<span>*</span></label>
                 <input type="text" name="usrNom" id="usrNom" required="required">
-              </p>
+
 
               <!-- ingreso de telefono -->
-              <p>
+
                 <label for="tel">Tel:<span>*</span></label>
                 <input type="text" name="tel" id="tel" required="required">
 
@@ -40,36 +40,39 @@
 
                 <label for="cel">Cel:</label>
                 <input type="text" name="cel" id="cel">
-              </p>
+
 
               <!-- ingreso de Domicilio -->
-              <p>
+
                 <label for="dom">Domicilio:<span>*</span></label>
                 <input type="text" name="dom" id="dom" required="required">
+
+
 
                 <label for="colonia">Colonia:<span>*</span></label>
                 <input type="text" name="colonia" id="colonia" required="required">
 
+
+
                 <label for="cp">CP:<span>*</span></label>
                 <input type="text" name="cp" id="cp" required="required">
-              </p>
+
 
               <!-- ingreso de correo electronico -->
-              <p>
+
                 <label for="correo">e-mail:<span>*</span></label>
                 <input type="email" name="correo" id="correo" required="required" placeholder="usuario@ejemplo.com">
-              </p>
+
 
               <!-- Contraseña -->
-              <p>
+
                 <label for="clave">Contraseña:<span>*</span></label>
                 <input type="password" name="clave" id="clave" required="required">
-                <!-- Confirmar contraseña -->
-                <br>
+              <!-- Confirmar contraseña -->
+
+
                 <label for="cclave">Confirmar contraseña:<span>*</span></label>
                 <input type="password" name="cclave" id="cclave" required="required">
-              </p>
-
               <!-- guardar datos -->
               <button type="button" name="btG" id="btG" onclick="validarForm()">Guardar</button>
               <!-- calcelar registro -->
