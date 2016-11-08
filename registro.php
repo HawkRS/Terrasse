3bc2e5166d08808925e7cfd403dbf7244cda0eda
DanteCero <zeroakron@gmail.com>
2016-11-07 21:59:34 -0600

    Los elementos del formulario se agregaron en parrafos para cambiar el estilo, tambien se agrego las entradas de código postal y colonia

--- registro.php
+++ registro.php
@@ -12,41 +12,63 @@
 
     <div id="envoltura">
       <div id="contenedor">
+
         <div id="cabecera">
-          <img src="img/logos/terrasse.png" alt="logo de la pagina" style="width: 200px"/>
+          <img src="img/logos/terrasse.png" alt="Articulos para terrazas" style="width: 200px"/>
         </div>
+
         <div id="cuerpo">
             <form method="post" id="nvoUsr">
               <!-- Ingreso del nombre del usuario -->
-              <label for="nombre">Nombre:<span>*</span></label>
-              <input type="text" name="nombre" id="nombre" required="required">
+              <p>
+                <label for="nombre">Nombre:<span>*</span></label>
+                <input type="text" name="nombre" id="nombre" required="required">
+              </p>
 
               <!-- Nombre de usuario para entrar al sistema -->
-              <label for="usrNom">Nombre de usuario:<span>*</span></label>
-              <input type="text" name="usrNom" id="usrNom" required="required">
+              <p>
+                <label for="usrNom">Nombre de usuario:<span>*</span></label>
+                <input type="text" name="usrNom" id="usrNom" required="required">
+              </p>
 
               <!-- ingreso de telefono -->
-              <label for="tel">Tel:<span>*</span></label>
-              <input type="text" name="tel" id="tel" required="required">
+              <p>
+                <label for="tel">Tel:<span>*</span></label>
+                <input type="text" name="tel" id="tel" required="required">
 
               <!-- ingreso de número celular -->
-              <label for="cel">Cel:</label>
-              <input type="text" name="cel" id="cel">
+
+                <label for="cel">Cel:</label>
+                <input type="text" name="cel" id="cel">
+              </p>
 
               <!-- ingreso de Domicilio -->
-              <label for="dom">Domicilio:<span>*</span></label>
-              <input type="text" name="dom" id="dom" required="required">
+              <p>
+                <label for="dom">Domicilio:<span>*</span></label>
+                <input type="text" name="dom" id="dom" required="required">
+
+                <label for="colonia">Colonia:<span>*</span></label>
+                <input type="text" name="colonia" id="colonia" required="required">
+
+                <label for="cp">CP:<span>*</span></label>
+                <input type="text" name="cp" id="cp" required="required">
+              </p>
 
               <!-- ingreso de correo electronico -->
-              <label for="correo">e-mail:<span>*</span></label>
-              <input type="email" name="correo" id="correo" required="required" placeholder="usuario@ejemplo.com">
+              <p>
+                <label for="correo">e-mail:<span>*</span></label>
+                <input type="email" name="correo" id="correo" required="required" placeholder="usuario@ejemplo.com">
+              </p>
 
               <!-- Contraseña -->
-              <label for="clave">Contraseña:<span>*</span></label>
-              <input type="password" name="clave" id="clave" required="required">
-              <!-- Confirmar contraseña -->
-              <label for="cclave">Confirmar contraseña:<span>*</span></label>
-              <input type="password" name="cclave" id="cclave" required="required">
+              <p>
+                <label for="clave">Contraseña:<span>*</span></label>
+                <input type="password" name="clave" id="clave" required="required">
+                <!-- Confirmar contraseña -->
+                <br>
+                <label for="cclave">Confirmar contraseña:<span>*</span></label>
+                <input type="password" name="cclave" id="cclave" required="required">
+              </p>
 
               <!-- guardar datos -->
               <button type="button" name="btG" id="btG" onclick="validarForm()">Guardar</button>
