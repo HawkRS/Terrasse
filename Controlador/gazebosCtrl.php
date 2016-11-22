7b4c01f6efb41dd4a05d5c8dbf5ba360d7b87096
DanteCero <zeroakron@gmail.com>
2016-11-21 21:56:18 -0600

    Solo se manda llamar a la función para mostrar la vista de la pagina por el momento

---
+++ Controlador/gazebosCtrl.php
@@ -0,0 +1,18 @@
+<?php
+  class gazebosCtrl
+  {
+    public function ejecutar()
+    {
+      $this->mostrar();
+    }
+
+    public function mostrar()
+    {
+      $head = file_get_contents("Vista/head.html");
+      $vista = file_get_contents("Vista/gazebo.html");
+      $footer = file_get_contents("Vista/footer.html");
+
+      echo $head.$vista.$footer;
+    }
+  }
+?>
