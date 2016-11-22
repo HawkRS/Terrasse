ff55b85ca0cac2101121df57ef490a319173dfee
DanteCero <zeroakron@gmail.com>
2016-11-21 21:46:09 -0600

    Archivo index para cargar los controladores de cada sección del sitio web

--- index.php
+++ index.php
@@ -1,99 +1,61 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+  session_start();
 
-  <head>
-    <meta charset="utf-8">
-    <title>TERRASSE</title>
-    <link rel="stylesheet" href="css/carrusel.css" type="text/css" media="screen">
-    <?php include 'head.php' ?>
-  </head>
+  if(isset($_GET) && !empty($_GET['ctrl']))
+  {
+    $controlador = $_GET['ctrl'];
+    switch($controlador)
+    {
+      case '':
+      case 'inicio':
+        require_once('Controlador/inicioCtrl.php');
+        $controlador = new inicioCtrl();
+        break;
 
-  <body>
-    <!-- Comienza el corrusel de imágnes-->
-    <div class="contenedor">
-      <div class="slides">
-        <img src="img/carrusel/galeria/01.jpg" alt="#" />
-        <img src="img/carrusel/galeria/02.jpg" alt="#" />
-        <img src="img/carrusel/galeria/03.jpg" alt="#" />
-      </div>
-    </div>
+      case 'asadores':
+        require_once('Controlador/asadoresCtrl.php');
+        $controlador = new asadoresCtrl();
+        break;
 
-    <div class="info">
-      <!--INICIA BLOQUE PRINCIPAL-->
-    <div class="bloque">
-      <!--INICIA SECCION PRINCIPAL-->
-      <section class="MSection">
-        <!--TERMINA GRID DE ARTICULOS-->
-        <div class="articulos">
-          <div class="AG1">
-            <div class="ART">
-              <img class="AIma" src="img/ofertas/escala/XYimg2.png" alt="Asadores" id="AIma"/>
-              <li class="MLink"><a href="asadores.php">ASADORES</a></li>
-            </div>
-            <div class="ART">
-              <img class="AIma" src="img/ofertas/escala/XYimg2.png" alt="Firepit" id="AIma"/>
-              <li class="MLink"><a href="firepit.php">FIREPIT</a></li>
-            </div>
-          </div>
-          <div class="AG1">
-            <div class="ART">
-              <img class="AIma" src="img/ofertas/escala/XYimg2.png" alt="Asadores" id="AIma"/>
-              <li class="MLink"><a href="gazebo.php">GAZEBOS</a></li>
-            </div>
-            <div class="ART">
-              <img class="AIma" src="img/ofertas/escala/XYimg2.png" alt="Firepit" id="AIma"/>
-              <li class="MLink"><a href="accesorios.php">ACCESORIOS</a></li>
-            </div>
-          </div>
-        </div>
+      case 'firepit':
+        require_once('Controlador/firepitCtrl.php');
+        $controlador = new firepitCtrl();
+        break;
 
-          <!--INICIA INFO DE LA EMPRESA-->
-          <div>
-            <div class="acerca">
-              <h4>ACERCA DE NOSOTROS</h4>
-              <p class="MPInfo">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-            <div class="compranos">
-              <h4>PORQUE ELEGIR A TERRASSE</h4>
-              <p class="MPInfo">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-          </div>
-        </section>
-      </div>
+      case 'gazebos':
+        require_once('Controlador/gazebosCtrl.php');
+        $controlador = new gazebosCtrl();
+        break;
 
-      <!--Script para activar el carrusel-->
-      <script src="js/jquery.js"></script>
-  		<script src="js/jquery.slides.js"></script>
-  		<script>
+      case 'accesorios':
+        require_once('Controlador/accesoriosCtrl.php');
+        $controlador = new accesoriosCtrl();
+        break;
 
-  				$(function(){
-  				$(".slides").slidesjs({
-  					play: {
-  						active: true,
-  						effect: "slide",
-  						interval: 3000,
-  						auto: true,
-  						swap: true,
-  						pauseOnHover: false,
-  						restartDelay: 2500
-  				    }
-  				});
-  				});
-  		</script>
-    </div>
-  </body>
+      case 'contacto':
+        require_once('Controlador/contactoCtrl.php');
+        $controlador = new contactoCtrl();
+        break;
 
-  <footer class="clear">
-    <?php include 'footer.php'; ?>
-  </footer>
-</html>
+      case 'usuario':
+        require_once('Controlador/usuarioCtrl.php');
+        $controlador = new usuarioCtrl();
+        break;
+
+      case 'equipo':
+        require_once('Controlador/equipoCtrl.php');
+        $controlador = new equipoCtrl();
+        break;
+
+      default: http_response_code(404);
+    }
+
+    $controlador->ejecutar();
+  }
+  else
+  {
+    require_once('Controlador/inicioCtrl.php');
+    $controlador = new inicioCtrl();
+    $controlador->ejecutar();
+  }
+?>
