0e7de63cf952cc87ebf904af0df740cb1717822a
DanteCero <zeroakron@gmail.com>
2016-11-23 23:26:37 -0600

    se guardan las entradas del registro del formulario con la función agregar usuario

--- Controlador/usuarioCtrl.php
+++ Controlador/usuarioCtrl.php
@@ -1,6 +1,7 @@
 <?php
 
-  class usuarioCtrl
+  require_once('ctrlEstandar.php');
+  class usuarioCtrl extends ctrlEstandar
   {
     public function ejecutar()
     {
@@ -24,7 +25,23 @@
 
     public function agregarUsr()
     {
-
+      print_r($_POST);
+      if(isset($_POST['nombre']) && isset($_POST['apellidoP']) && isset($_POST['apellidoM'])
+			&& isset($_POST['usrNom']) && isset($_POST['tel']) 	     && isset($_POST['cel'])
+			&& isset($_POST['dom'])    && isset($_POST['colonia']) 	 && isset($_POST['cp'])
+			&& isset($_POST['correo']) && isset($_POST['clave'])     && isset($_POSRT['cclave']))
+      {
+        $nombre = $_POST['nombre'];
+        $apellidoP = $_POST['apellidoP'];
+        $apellidoM = $_POST['apellidoM'];
+        $usrNom = $_POST['usrNom'];
+        $tel = $_POST['tel'];
+        $cel = $_POST['cel'];
+        $dom = $_POST['dom'];
+        $correo = $_POST['correo'];
+        $clave = $_POST['clave'];
+        $cclave = $_POST['cclave'];
+      }
     }
   }
 
