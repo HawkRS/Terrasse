06b37320041bbc7b4eeb079d922d1166464a7563
DanteCero <zeroakron@gmail.com>
2016-12-04 15:45:33 -0600

    se corrigió en error al modelo porque en el navegador mostraba error 500

--- Controlador/carritoCtrl.php
+++ Controlador/carritoCtrl.php
@@ -8,7 +8,7 @@
     function __construct()
     {
       require_once('./Modelo/carritoMdl.php');
-      $this->model = new carritoMdl();
+      $this->modelo = new carritoMdl();
     }
 
     public function ejecutar()
