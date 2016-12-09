3927642a7b8637b3b34805065fda93908365e087
DanteCero <zeroakron@gmail.com>
2016-12-08 23:50:22 -0600

    se agrego la función ejecutar para duando se necesite hacer alguna consulta

--- Modelo/baseDts.php
+++ Modelo/baseDts.php
@@ -8,7 +8,11 @@
     private $clave = CLAVE;
     private $servidor = SERVIDOR;
     private $base_dts = BASE_DTS;
+
+    private $resultado = array();
+    private $contador = 0;
     private $conexion;
+    private $consulta;
 
     public function __construct()
     {
@@ -43,6 +47,34 @@
     public function desconectar()
     {
       mysql_close($this->conexion);
+    }
+
+    public function ejecutar($accion)
+    {
+      $this->consulta = $this->conexion->query($accion);
+
+      if($this->consulta)
+      {
+        if(is_object($this->consulta))
+        {
+          while($fila = $this->consulta->fetch_assoc())
+            $this->resultado[] = $fila;
+
+          $this->contador = $this->consulta->num_rows;
+        }
+      }
+      else
+      {
+        die($this->conexion->error);
+        return false;
+      }
+
+      return $this;
+    }
+
+    public function resultado()
+    {
+      return $this->resultado;
     }
   }
 ?>
