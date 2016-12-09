0c6527055d1623f51942a03c577c66002a790072
DanteCero <zeroakron@gmail.com>
2016-12-08 21:58:52 -0600

    Se agregó al menú la opción para mostrar las vistas de los productos de la página

--- Controlador/productoCtrl.php
+++ Controlador/productoCtrl.php
@@ -7,6 +7,26 @@
       $accion = $_GET['act'];
       switch($accion)
       {
+        case 'v_Asadores':
+          $this->mostrar("Terrasse | Asadores","Vista/asadores.html");
+          break;
+
+        case 'v_Firepit':
+          $this->mostrar("Terrasse | Firepit","Vista/firepit.html");
+          break;
+
+        case 'v_Gazebos':
+          $this->mostrar("Terrasse | Gazebos","Vista/gazebo.html");
+          break;
+
+        case 'v_Accesorios':
+          $this->mostrar("Terrasse | Accesorios","Vista/accesorios.html");
+          break;
+
+        case 'v_Productos':
+          $this->mostrar("Terrasse | Productos","Vista/productos.html");
+          break;
+
         case 'registrar':
           if(empty($_POST))
             echo file_get_contents('Vista/formularioProducto.html');
@@ -30,6 +50,15 @@
          $descripcion = $_POST['descripcion'];
          $imagen = $_POST['imgProducto'];
        }
+    }
+
+    public function mostrar($titulo,$ruta)
+    {
+      $head = $this->head($titulo);
+      $vista = file_get_contents($ruta);
+      $footer = file_get_contents("Vista/footer.html");
+
+      echo $head.$vista.$footer;
     }
   }
 ?>
